448377dd4163701d0f62bfcfa1cf44fde1b42386
Izak Hudnik <hudnik.izak@gmail.com>
2023-05-12 23:07:30 +0200

    Added Book I Definition 4

--- Book_I/Book_I_Definitions.py
+++ Book_I/Book_I_Definitions.py
@@ -46,10 +46,23 @@ class Book_I_Definitions(Scene):
 
         self.clear()
 
+        # Display fourth definition
+
+        Definicija_II = "Knjiga I, Definicija IV."
+        prop = "Ravna črta leži enakomerno\nmed svojima koncema."
+
+        
+        p.display_text(self, Definicija_II, prop)
+
+        Book_I_Definitions.Definition_IV(self, point1=line.start, point2=line.end)
+        self.wait()
+
+        self.clear()
+
 
     def Definition_I(scene : Scene, point : Point = ORIGIN, color : Color = WHITE):
         """
-        "First Axiom: A point is that which has not parts"
+        "First Definition: A point is that which has not parts"
         Takes in a scene : Scene, a point : Point and color : Color
         Plays the creation of a dot at the coordinates of the point
         """
@@ -58,9 +71,9 @@ class Book_I_Definitions(Scene):
 
         return dot
 
-    def Definition_II(scene : Scene, start : Point = LEFT, end : Point = RIGHT, color : Color = WHITE):
+    def Definition_II(scene : Scene, start : Point = 2 * LEFT, end : Point = 2 * RIGHT, color : Color = WHITE):
         """
-        Second Axiom: A line is length without breath.
+        Second Definition: A line is length without breath.
         Takes in scene : Scene, start and end of line : Point, and color : Color
         Plays the creation of a line from start to end
         """
@@ -71,7 +84,7 @@ class Book_I_Definitions(Scene):
 
     def Definition_III(scene : Scene, line : Line, first_dot_color = WHITE, second_dot_color = WHITE):
         """
-        Second Axiom: The extremities of a line are points.
+        Third Definition: The extremities of a line are points.
         Takes in scene : Scene, line : Line, and first_dot_color and second_dot_color : Color
         Plays the creation of a line from start to end
         """
@@ -79,7 +92,7 @@ class Book_I_Definitions(Scene):
         dot1 = Dot(line.start, color=line.color)
         dot2 = Dot(line.end, color=line.color)
 
-        scene.play(Create(line))
+        scene.add(line)
         scene.play(Create(dot1))
         scene.play(Create(dot2))
 
@@ -87,3 +100,26 @@ class Book_I_Definitions(Scene):
         line.add(dot2)
 
         return line
+    
+    def Definition_IV(scene : Scene, point1 : Point, point2 : Point, color : Color = WHITE):
+        """
+        "Fourth Definition: A straight line is that which lies evenly between its extremities."
+        Takes in (a) scene : Scene, (b) point1 and (c) point2 : Point, color : Color 
+        """
+        dot1 = Dot(point1, color=color)
+        dot2 = Dot(point2, color=color)
+        line = Line(start=point1, end=point2)
+
+        scene.add(dot1)
+        scene.add(dot2)
+        scene.play(Create(line))
+
+        line.add(dot1)
+        line.add(dot2)
+
+        return line
+    
+    def Definition_V():
+        """
+        Fifth definition: A surface is that which has length and breadth only.
+        """
